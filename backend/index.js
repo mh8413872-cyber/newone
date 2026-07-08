e5adc3a09444e06e6b782dbfff4adeb0448f96e8
mirza husnain <mh8413872gmail..com>
2026-07-08 14:12:09 +0500

    asas

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,31 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+
+// const AuthRouter = require('./Routes/AuthRouter');
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());``
+
+// mongoose.connect(
+//     'mongoose.connect(process.env.MONGO_URI);'
+// )
+// .then(() => console.log('MongoDB Connected'))
+// .catch(err => console.log(err));
+
+// app.use('/auth', AuthRouter);
+
+// app.listen(8000, () => {
+//     console.log('Server running on 8000');
+// });
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const AuthRouter = require('./Routes/AuthRouter');
 
@@ -9,14 +34,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(
-    'mongodb://127.0.0.1:27017/jwtDB'
-)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+mongoose.connect('mongodb+srv://husnain:husnain123@cluster0.0awkwmh.mongodb.net/?appName=Cluster0')
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
 
 app.use('/auth', AuthRouter);
 
-app.listen(8000, () => {
-    console.log('Server running on 8000');
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
